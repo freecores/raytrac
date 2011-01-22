e2283d4b1128dee668155052157c9531f56d5717
jguarin2002 <jguarin2002@4be94b91-392e-49eb-a80e-b673118448a5>
2011-01-22 22:44:29 +0000

    syntax typo fixed...

--- uf.vhd
+++ uf.vhd
@@ -24,7 +24,7 @@ entity uf is
 	port (
 		opcode		: in std_logic;
 		m0f0,m0f1,m1f0m1f1,m2f0,m2f1,m3f0,m3f1,m4f0,m4f1,m5f0,m5f1 : in std_logic_vector(17 downto 0);
-		cpx,cpy,cpz,dp0,dp1 : out std_logic_vector(31 downto 0)
+		cpx,cpy,cpz,dp0,dp1 : out std_logic_vector(31 downto 0);
 		clk,rst		: in std_logic
 	);
 end uf;

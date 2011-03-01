a9a77aa8dbb7c7dcdc8f7b1143527a1c38a339a8
jguarin2002 <jguarin2002@4be94b91-392e-49eb-a80e-b673118448a5>
2011-03-01 11:38:26 +0000

    Tb ggodies

--- clock_gen.vhd
+++ clock_gen.vhd
@@ -3,7 +3,7 @@
 --! @author Julian Andres Guarin Reyes.
 -- RAYTRAC
 -- Author Julian Andres Guarin
--- fastmux.vhd
+-- clockgen.vhd
 -- This file is part of raytrac.
 -- 
 --     raytrac is free software: you can redistribute it and/or modify
@@ -18,3 +18,29 @@
 -- 
 --     You should have received a copy of the GNU General Public License
 --     along with raytrac.  If not, see <http://www.gnu.org/licenses/>.
+library ieee;
+use ieee.std_logic_1164.all;
+use work.arithpack.all;
+
+entity clock_gen is
+	generic	(tclk : time := 20 ns);
+	port	(clk,rst : out std_logic);
+end entity clock_gen;
+
+architecture clock_gen_arch of clock_gen is
+
+	constant 
+
+begin
+	resetproc: process
+	begin
+		rst<= '1';
+		wait for 50 ns;
+		rst<= '0';
+		wait;
+	end process;
+	clockproc: process
+	
+		
+
+
